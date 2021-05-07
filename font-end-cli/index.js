0cdda9528e2cfbd9c8f6ebccaf8e9cbe5cf968ca
wangzhenchuan <itclancode@163.com>
2021-05-07 17:48:20 +0800

    添加前端脚手架工具

---
+++ font-end-cli/index.js
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+
+const program = require('commander');
+const download = require('download-git-repo');
+
+//version 版本号
+//name 新项目名称
+program.version('1.0.0', '-v, --version')
+    .command('init <templateName> <projectName>')
+    .action((templateName, projectName) => {
+        if (templateName === "vue") {
+            console.log('clone template ...');
+            download('github:itclancode/code-cli-vue2.0-temp-src', projectName, function (err) {
+                console.log(err ? 'Error' : 'Success')
+            })
+        } else if(templateName === "react") {
+            console.log('clone template ...');
+            download('github:itclancode/code-cli-react-temp-src', projectName, function (err) {
+                console.log(err ? 'Error' : 'Success')
+            })
+        } else {
+          console.error('A template name that does not exist')
+        }
+    });
+program.parse(process.argv);
